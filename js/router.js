78a4b1578e21844adf2fcc56b8d6ec82c783e0ff
Brad Rodriguez <brodrigu@gmail.com>
2013-06-18 00:20:36 -0700

    Adding join and new game routes

--- js/router.js
+++ js/router.js
@@ -2,6 +2,9 @@ Bngo.Router.map(function () {
   this.resource('games', { path: '/' }, function () {
     // additional child routes
   });
+
+  this.route('newGame');
+  this.route('joinGame');
 });
 
 Bngo.GamesRoute = Ember.Route.extend({
@@ -11,6 +14,18 @@ Bngo.GamesRoute = Ember.Route.extend({
 });
 
 Bngo.GamesIndexRoute = Ember.Route.extend({
+  model: function () {
+    return Bngo.Game.find();
+  }
+});
+
+Bngo.NewGameRoute = Ember.Route.extend({
+  model: function () {
+    return Bngo.Game.find();
+  }
+});
+
+Bngo.JoinGameRoute = Ember.Route.extend({
   model: function () {
     return Bngo.Game.find();
   }
